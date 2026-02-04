5738a8c4a82be1fef85d6168643a61eed9d5247d
Mohai <mohai.cse@gmail.com>
2026-02-04 19:30:11 +0600

    Add project files.

---
+++ ParaBank_Playwright_DotNet/Factories/PlaywrightFactory.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParaBank_Playwright_DotNet.Factories
+{
+    public class PlaywrightFactory
+    {
+
+        public IPlaywright playwright = default!;
+        public IBrowser browser = default!;
+        public IBrowserContext context = default!;
+        public IPage page = default!;
+
+        public async Task<IPage> InitBrowser(string browserName)
+        {
+            Console.WriteLine("Browser name: " + browserName);
+
+            playwright = await Playwright.CreateAsync();
+
+            switch (browserName.ToLower())
+            {
+                case "chrome":
+                    browser = await playwright.Chromium.LaunchAsync(new()
+                    {
+                        Headless = false,
+                        Channel = "chrome"
+
+                    });
+                    break;
+
+                case "edge":
+                    browser = await playwright.Chromium.LaunchAsync(new()
+                    {
+                        Headless = false,
+                        Channel = "msedge"
+
+                    });
+                    break;
+
+                case "firefox":
+                    browser = await playwright.Firefox.LaunchAsync(new()
+                    {
+                        Headless = false
+                    });
+                    break;
+
+                case "safari":
+                    browser = await playwright.Webkit.LaunchAsync(new()
+                    {
+                        Headless = false
+                    });
+                    break;
+
+                case "chromium":
+                    browser = await playwright.Chromium.LaunchAsync(new()
+                    {
+                        Headless = false
+                    });
+                    break;
+
+                default:
+                    Console.WriteLine("Incorrect Browser Name.");
+                    break;
+            }
+
+            context = await browser.NewContextAsync();
+            page = await browser.NewPageAsync();
+
+            await page.GotoAsync("https://parabank.parasoft.com/parabank/index.htm");
+
+            return page;
+
+        }
+
+    }
+}
---
+++ ParaBank_Playwright_DotNet/Pages/LoginPage.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParaBank_Playwright_DotNet.Pages
+{
+    public class LoginPage
+    {
+        public IPage page = default!;
+
+        private readonly string usernameLoc = "//input[@name='username']";
+        private readonly string passwordLoc = "//input[@name='password']";
+        private readonly string loginButtonLoc = "//input[@value='Log In']";
+
+        public LoginPage(IPage page)
+        {
+            this.page = page;
+        }
+
+        public async Task EnterUsername(string username)
+        {
+            await page.Locator(usernameLoc).FillAsync(username);
+        }
+
+        public async Task EnterPassword(string password)
+        {
+            await page.Locator(passwordLoc).FillAsync(password);
+        }
+
+        public async Task ClickOnLoginButton()
+        {
+            await page.Locator(loginButtonLoc).ClickAsync();
+        }
+
+    }
+}
