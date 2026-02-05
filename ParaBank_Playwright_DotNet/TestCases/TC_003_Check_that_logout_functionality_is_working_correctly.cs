702adf89b756c2d8ad50c8c6547aa573e46625b8
Mohai <mohai.cse@gmail.com>
2026-02-05 23:32:46 +0600

    TC_003 Logout is in progress

---
+++ ParaBank_Playwright_DotNet/TestCases/TC_003_Check_that_logout_functionality_is_working_correctly.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParaBank_Playwright_DotNet.TestCases
+{
+    internal class TC_003_Check_that_logout_functionality_is_working_correctly:BaseTest
+    {
+    }
+}
