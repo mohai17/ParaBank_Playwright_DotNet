9247d5261240ffbc32648ef7417f3ebf993c0123
Mohai <mohai.cse@gmail.com>
2026-02-06 22:17:39 +0600

    JsonUtil is updated

--- ParaBank_Playwright_DotNet/Utils/JsonUtil.cs
+++ ParaBank_Playwright_DotNet/Utils/JsonUtil.cs
@@ -2,42 +2,41 @@
 using System.IO;
 using System.Text.Json;
 
-namespace ParaBank_Playwright_DotNet.Utils
+namespace ProjectUtilityJSON;
+
+public class BrowserSettings
 {
-    public class BrowserSettings
-    {
-        public string? BrowserName { get; set; }
-        public bool Headless { get; set; }
-        public int Slowmotion { get; set; }
-        public int Timeout { get; set; }
-        public string? URL { get; set; }
-    }
+    public string? BrowserName { get; set; }
+    public bool Headless { get; set; }
+    public int Slowmotion { get; set; }
+    public int Timeout { get; set; }
+    public string? URL { get; set; }
+}
 
-    public class Config
-    {
-        public BrowserSettings? BrowserSettings { get; set; }
-    }
+public class Config
+{
+    public BrowserSettings? BrowserSettings { get; set; }
+}
 
-    internal class JSONUtil
-    {
-        private readonly string _configPath =
-            "C:\\Users\\mohai\\source\\repos\\ParaBank_Playwright_DotNet\\ParaBank_Playwright_DotNet\\Resources\\Config\\BrowserConfig.json";
+internal class JSONUtil
+{
+    private readonly string _configPath =
+        "C:\\Users\\mohai\\source\\repos\\ParaBank_Playwright_DotNet\\ParaBank_Playwright_DotNet\\Resources\\Config\\BrowserConfig.json";
 
-        public Config LoadConfig()
+    public Config LoadConfig()
+    {
+        try
         {
-            try
-            {
-                string jsonFile = File.ReadAllText(_configPath);
+            string jsonFile = File.ReadAllText(_configPath);
 
-                Config? jsonData = JsonSerializer.Deserialize<Config>(jsonFile);
+            Config? jsonData = JsonSerializer.Deserialize<Config>(jsonFile);
 
-                return jsonData ?? new Config();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error loading config: {ex.Message}");
-                return new Config();
-            }
+            return jsonData ?? new Config();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error loading config: {ex.Message}");
+            return new Config();
         }
     }
 }
