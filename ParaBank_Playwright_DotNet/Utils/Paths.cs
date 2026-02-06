89377ef5290ad0a1a1549c902a0f88d8d98a8fd6
Mohai <mohai.cse@gmail.com>
2026-02-06 22:19:17 +0600

    Paths class and ScreenShotHelper is added

---
+++ ParaBank_Playwright_DotNet/Utils/Paths.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+using System.Xml.Linq;
+
+namespace ProjectUtilityPaths
+{
+    public static class Paths
+    {
+
+        public static string DataXLSXPath(string XLName)
+        {
+            string projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\.."));
+            string localfilePath = Path.Combine(projectRoot, "TestData", $"{XLName}");
+
+            return localfilePath;
+        }
+
+
+    }
+}
---
+++ ParaBank_Playwright_DotNet/Utils/ScreenShotHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ProjectUtilityScreenShot;
+internal class ScreenshotHelper
+{
+    public static async Task<string> TakeScreenshotAsync(IPage page, string screenshotName)
+    {
+        string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "Screenshots");
+        Directory.CreateDirectory(folderPath);
+
+        string filePath = Path.Combine(
+            folderPath,
+            $"{screenshotName}_{DateTime.Now:yyyyMMdd_HHmmss}.png"
+        );
+
+        byte[] bytes = await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = filePath,
+            FullPage = true
+        });
+
+        return Convert.ToBase64String(bytes);
+    }
+}
