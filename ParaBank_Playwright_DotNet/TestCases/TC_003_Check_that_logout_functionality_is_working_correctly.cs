c25d728b9701009b3b0bf9769d887b7a89ecbe40
Mohai <mohai.cse@gmail.com>
2026-02-08 20:26:54 +0600

    TC_003 DDT is done.

--- ParaBank_Playwright_DotNet/TestCases/TC_003_Check_that_logout_functionality_is_working_correctly.cs
+++ ParaBank_Playwright_DotNet/TestCases/TC_003_Check_that_logout_functionality_is_working_correctly.cs
@@ -1,4 +1,6 @@
 ﻿using ParaBank_Playwright_DotNet.Pages;
+using ProjectUtilityExcel;
+using ProjectUtilityPaths;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,13 +9,21 @@ namespace ParaBank_Playwright_DotNet.TestCases
 {
     internal class TC_003_Check_that_logout_functionality_is_working_correctly:BaseTest
     {
+
+        private readonly string excelpath = Paths.DataXLSXPath("ParaBankTestData.xlsx");
+
         [Test]
         public async Task TS_001_user_wants_to_logout()
         {
+            ExcelReaderUtil.PopulateInCollection(excelpath, "RegisterData");
+            var rowNumber = 1;
+            string username = ExcelReaderUtil.ReadData(rowNumber, "Username") ?? string.Empty;
+            string password = ExcelReaderUtil.ReadData(rowNumber, "Password") ?? string.Empty;
+
             LoginPage login = new LoginPage(page);
 
-            await login.EnterUsername("aaa");
-            await login.EnterPassword("aaaaaa");
+            await login.EnterUsername(username);
+            await login.EnterPassword(password);
             await login.ClickOnLoginButton();
 
             Dashboard dash = new Dashboard(page);
