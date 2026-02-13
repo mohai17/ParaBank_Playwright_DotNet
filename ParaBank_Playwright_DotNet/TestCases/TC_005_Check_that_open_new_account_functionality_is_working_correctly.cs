074ce6c8df74cfa010772678d3a23e5526f8d827
Mohai <mohai.cse@gmail.com>
2026-02-13 14:57:53 +0600

    TC_005 is added and in progress

---
+++ ParaBank_Playwright_DotNet/TestCases/TC_005_Check_that_open_new_account_functionality_is_working_correctly.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParaBank_Playwright_DotNet.TestCases
+{
+    internal class TC_005_Check_that_open_new_account_functionality_is_working_correctly
+    {
+    }
+}
