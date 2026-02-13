bdab137ddd3b149fa356113c35f16ca518c45845
Mohai <mohai.cse@gmail.com>
2026-02-13 14:57:12 +0600

    TC_004 is add and in progress

---
+++ ParaBank_Playwright_DotNet/TestCases/TC_004_Check_that_find_login_info_functionality_is_working_correctly.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParaBank_Playwright_DotNet.TestCases
+{
+    internal class TC_004_Check_that_find_login_info_functionality_is_working_correctly
+    {
+    }
+}
