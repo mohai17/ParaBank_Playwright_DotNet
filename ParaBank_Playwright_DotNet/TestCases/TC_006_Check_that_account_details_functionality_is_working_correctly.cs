df08fba90fe0c13eed24ce2e8b8811cfa365c776
Mohai <mohai.cse@gmail.com>
2026-02-13 14:58:29 +0600

    TC_006 is added and in progress

---
+++ ParaBank_Playwright_DotNet/TestCases/TC_006_Check_that_account_details_functionality_is_working_correctly.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParaBank_Playwright_DotNet.TestCases
+{
+    internal class TC_006_Check_that_account_details_functionality_is_working_correctly
+    {
+    }
+}
