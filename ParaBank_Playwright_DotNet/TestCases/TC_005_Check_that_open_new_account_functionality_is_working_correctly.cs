844be015b3d25f3e5c0b3beee60ddf2e29dda4bd
Mohai <mohai.cse@gmail.com>
2026-02-14 14:47:22 +0600

    TC_005 is done.

--- ParaBank_Playwright_DotNet/TestCases/TC_005_Check_that_open_new_account_functionality_is_working_correctly.cs
+++ ParaBank_Playwright_DotNet/TestCases/TC_005_Check_that_open_new_account_functionality_is_working_correctly.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using ParaBank_Playwright_DotNet.Pages;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ParaBank_Playwright_DotNet.TestCases
 {
-    internal class TC_005_Check_that_open_new_account_functionality_is_working_correctly
+    internal class TC_005_Check_that_open_new_account_functionality_is_working_correctly:BaseTest
     {
+        [Test]
+        public async Task TS_001_user_wants_to_open_a_new_account()
+        {
+
+            LoginPage login = new LoginPage(page);
+            await login.EnterUsername("parasoft");
+            await login.EnterPassword("demo");
+            await login.ClickOnLoginButton();
+
+            OpenNewAccountPage openAcc = new OpenNewAccountPage(page);
+
+            await openAcc.ClickOnOpenNewAccountLink();
+            await openAcc.SelectAccountType("SAVINGS");
+            await openAcc.SelectFromAccId(0);
+            await openAcc.ClickOnOpenAccountButton();
+            
+            bool actualResult = await openAcc.IsNewAccountNumberVisible();
+
+            Assert.That(actualResult, Is.True);
+        }
+
     }
 }
