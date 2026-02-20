038956f41466e13d692263a0b673dd2844adbd35
Mohai <mohai.cse@gmail.com>
2026-02-20 15:27:09 +0600

    Account Details Page Object is added and in progress

---
+++ ParaBank_Playwright_DotNet/Pages/AccountDetails.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParaBank_Playwright_DotNet.Pages
+{
+    
+    internal class AccountDetails
+    {
+
+        IPage page = default!;
+
+        public AccountDetails(IPage page)
+        {
+            this.page = page;
+        }
+
+
+
+    }
+}
